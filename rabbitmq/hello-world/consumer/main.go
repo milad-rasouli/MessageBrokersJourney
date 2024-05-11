0ebb382cb6aeaa4b962039a0ce145ab0e93a8185
Milad Rasouli <milad75rasouli@gmail.com>
2024-05-11 21:15:54 +0330

    chore: hello world publisher & consumer

--- rabbitmq/hello-world/consumer/main.go
+++ rabbitmq/hello-world/consumer/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Milad75Rasouli/MessageBrokersJourney/rabbitmq/helper"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	log.Println("RabbitMQ consumer is running")
+	connStr := "amqp://test:1234qwer@127.0.0.1:5672/cc-dev-vhost"
+
+	conn, err := amqp.Dial(connStr)
+	helper.FailOnError(err, "failed to connect to rabbitmq")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	helper.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	helper.FailOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	helper.FailOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
--- rabbitmq/hello-world/publisher/main.go
+++ rabbitmq/hello-world/publisher/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/Milad75Rasouli/MessageBrokersJourney/rabbitmq/helper"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	log.Println("RabbitMQ publisher is running")
+	connStr := "amqp://test:1234qwer@127.0.0.1:5672/cc-dev-vhost"
+
+	conn, err := amqp.Dial(connStr)
+	helper.FailOnError(err, "failed to connect to rabbitmq")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	helper.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	helper.FailOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Greeting!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	helper.FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
 }
